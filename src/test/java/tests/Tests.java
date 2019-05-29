3eef5f0b233880e3a9d7219e141e28129ba4fe75
ayshamarty <ayshamarty@gmail.com>
2019-05-29 14:21:12 +0100

    first commit

---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Tests {
+
+	public static WebDriver driver;
+
+	@Before
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Administrator\\Downloads\\chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		driver.manage().window().maximize();
+		driver.get("https://www.youidraw.com/apps/painter/");
+		Actions action = new Actions(driver);
+
+		action.moveByOffset(730, 500).clickAndHold().moveByOffset(50, -100).release().clickAndHold()
+				.moveByOffset(50, 100).release().moveByOffset(-80, -40).clickAndHold().moveByOffset(60, 0).release()
+				.perform();
+		action.moveByOffset(30, -60).clickAndHold().moveByOffset(40, 50).release().moveByOffset(30, -50).clickAndHold()
+				.moveByOffset(-60, 100).release().perform();
+		action.moveByOffset(130, -100).clickAndHold().moveByOffset(-60, 0).moveByOffset(0, 45).moveByOffset(60, 0)
+				.moveByOffset(0, 55).moveByOffset(-60, 0).release().perform();
+		action.moveByOffset(80, -100).clickAndHold().moveByOffset(0, 100).release().moveByOffset(0, -50).clickAndHold()
+				.moveByOffset(60, 0).release().moveByOffset(0, -50).clickAndHold().moveByOffset(0, 100).release()
+				.perform();
+		action.moveByOffset(10, 0).clickAndHold().moveByOffset(50, -100).release().clickAndHold().moveByOffset(50, 100)
+				.release().moveByOffset(-80, -40).clickAndHold().moveByOffset(60, 0).release().perform();
+		Thread.sleep(10000);
+
+	}
+}
